edcc77072f94c8afc6d29cbf606dacbf89f891b6
Jaysah02 <126542550+Jaysah02@users.noreply.github.com>
2025-08-01 18:37:56 +0530

    Update chatmodel_google.py

--- chatmodel_google.py
+++ chatmodel_google.py
@@ -4,7 +4,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
 
 # ✅ Insert your API key here
-os.environ["GOOGLE_API_KEY"] = "AIzaSyDEIcwsBNN95oKxy9cKW2RKO3It20KNI1M"
+os.environ["GOOGLE_API_KEY"] = "google api key"
 
 # ✅ Create the model instance
 model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')  # or 'gemini-1.5-pro' if supported
